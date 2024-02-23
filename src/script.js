432fc398a5d9cc23d257e61ac0dee0f42caff3bd
MPS Vihanha <>
2024-02-23 20:32:06 +0530

    event handling

--- src/script.js
+++ src/script.js
@@ -1,8 +1,24 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number !';
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
-document.querySelector('.number').textContent = 13;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = document.querySelector('.guess').value;
+  console.log(guess, typeof guess);
 
-document.querySelector('.score').textContent = 10;
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number !';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number !';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High !';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low !';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
